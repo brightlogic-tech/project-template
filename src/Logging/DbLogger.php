fd8e5923e76d387e2cfa128d5ffedc1149e86aa0
Václav Pelíšek <peldax@gmail.com>
2023-03-09 12:15:36 +0000

    Create DbLogger.php

---
+++ src/Logging/DbLogger.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Service;
+
+final class DbLogger implements \Tracy\ILogger
+{
+    public function __construct(
+        private \Infinityloop\Template\Logging\LogTable $logTable,
+    )
+    {
+    }
+
+    public function log(mixed $value, mixed $level = self::INFO) : void
+    {
+        $message = \Tracy\Logger::formatMessage($value);
+
+        try {
+            $this->logTable->insert([
+                'level' => $level,
+                'head' => \substr($message, 0, 200),
+                'message' => $message,
+            ]);
+        } catch (\Throwable $e) {
+            $stream = \fopen('php://stderr', 'wb');
+            \fwrite($stream, 'Unable to write following log to database:' . \PHP_EOL);
+            \fwrite($stream, $message . \PHP_EOL);
+            \fwrite($stream, 'Exception in database write:' . \PHP_EOL);
+            \fwrite($stream, $e->getMessage() . \PHP_EOL);
+            \fclose($stream);
+        }
+    }
+}
