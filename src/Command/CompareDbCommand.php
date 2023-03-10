50e03d1aae5a7704650a2c84d6713ed47dfed7c6
Václav Pelíšek <peldax@gmail.com>
2023-03-10 11:28:15 +0000

    Update CompareDbCommand.php

--- src/Command/CompareDbCommand.php
+++ src/Command/CompareDbCommand.php
@@ -12,6 +12,7 @@ final class CompareDbCommand extends \Symfony\Component\Console\Command\Command
     protected static $defaultName = 'compareDb';
 
     public function __construct(
+        private string $projectName,
         private \CoolBeans\Command\SqlGeneratorCommand $sqlGeneratorCommand,
     )
     {
@@ -46,7 +47,7 @@ final class CompareDbCommand extends \Symfony\Component\Console\Command\Command
         $app->add(new \Bref\Console\Command\Cli());
 
         $inputStr = new \Symfony\Component\Console\Input\StringInput(
-            'cli --region eu-central-1 nemovizor-dev-console -- sqlGenerator ./../app/Storage/Bean',
+            'cli --region eu-central-1 ' . $this->projectName . '-dev-console -- sqlGenerator ./../app/Storage/Bean',
         );
         $outputCapture = new class implements OutputInterface {
             /** @var array<string> */
